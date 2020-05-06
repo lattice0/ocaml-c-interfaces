066b7b896ba7c1236a6543263fb93ccdef6cb093
kakadu <kakadu.hafanana@gmail.com>
2020-05-06 12:09:04 +0300

    Makefiles + dumb stupid non-lwt threads

--- src/cpp/c_receive_events/event_emitter.ml
+++ src/cpp/c_receive_events/event_emitter.ml
@@ -1,9 +1,27 @@
 open Lwt.Infix
 
-external call: int64 -> string -> _ = "function_pointer_caller"
+external call: int -> string -> unit = "function_pointer_caller"
 
 let begin_event pointer = 
-    Lwt_unix.sleep 5.0 >>= fun () ->
-        call pointer "message"
+    Lwt_unix.sleep 1.0 >>= fun () ->
+        (Lwt.return @@ call pointer "message")
 
 let () = Callback.register "begin_event" begin_event
+
+
+let create_threads () =
+  print_endline "starting plain non-lwt threads";
+  let (_: Thread.t) =
+    let counter = ref 0 in
+    let rec f () =
+      let _ = Thread.delay 1.0 in
+      incr counter;
+      Printf.printf "ticked %d times\n%!" !counter;
+      f ()
+    in
+    Thread.create f ()
+  in
+  ()
+
+let () = create_threads()
+
